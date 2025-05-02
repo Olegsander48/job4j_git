68058106bb435ed3d3782bca1184f2a647d28979
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-05-02 11:07:43 +0300

    add get/set + toString

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -4,4 +4,37 @@ public class Car {
     private int year;
     private String mark;
     private String model;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "year=" + year +
+                ", mark='" + mark + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
 }
